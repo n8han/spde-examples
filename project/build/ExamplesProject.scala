82d5d48c2dae8230e3f3a27af0d022b86958efb9
Nathan Hamblen <nathan@technically.us>
2009-08-30 14:50:55 -0400

    rename to spde, use plugin

---
+++ project/build/ExamplesProject.scala
@@ -0,0 +1,11 @@
+import sbt._
+import spde._
+
+class ExamplesProject(info: ProjectInfo) extends ParentProject(info)
+{
+  lazy val explode = project("Explode", "Explode", new SpdeProject(_))  
+  lazy val flocking = project("Flocking", "Flocking", new SpdeProject(_))  
+  lazy val fold = project("Fold", "Fold", new SpdeProject(_))  
+  lazy val list = project("List", "List", new SpdeProject(_))
+  lazy val matrix = project("Matrix", "Matrix", new SpdeOpenGLProject(_))
+} 
