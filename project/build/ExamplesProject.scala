7e9e033cd792b709a991fb8f6d6cbe66cf6b852e
Nathan Hamblen <nathan@technically.us>
2009-09-22 02:46:24 -0400

    add Mark's Loop project, depend on plugin 0.1.3 for video

--- project/build/ExamplesProject.scala
+++ project/build/ExamplesProject.scala
@@ -9,4 +9,7 @@ class ExamplesProject(info: ProjectInfo) extends ParentProject(info)
   lazy val list = project("List", "List", new SpdeProject(_))
   lazy val gasket = project("Sierpinski", "Sierpinski_Gasket", new SpdeProject(_))
   lazy val matrix = project("Matrix", "Matrix", new SpdeOpenGLProject(_))
+
+  /* Video projects use GSVideo, which is hard to set up on the Mac. */
+  lazy val loop = project("Loop", "Loop", new SampleVideoProject(_))
 }
