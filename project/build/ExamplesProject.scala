2ff4a1fa09c5547927fc99db5ec0eb41a53f5d4b
Nathan Hamblen <nathan@technically.us>
2009-09-22 23:24:25 -0400

    add L-Systems example from http://www.scala-lang.org/node/3391

--- project/build/ExamplesProject.scala
+++ project/build/ExamplesProject.scala
@@ -8,6 +8,7 @@ class ExamplesProject(info: ProjectInfo) extends ParentProject(info)
   lazy val fold = project("Fold", "Fold", new SpdeProject(_))  
   lazy val list = project("List", "List", new SpdeProject(_))
   lazy val gasket = project("Sierpinski", "Sierpinski_Gasket", new SpdeProject(_))
+  lazy val lsystems = project("L-Systems", "L_Systems", new SpdeProject(_))
   lazy val matrix = project("Matrix", "Matrix", new SpdeOpenGLProject(_))
 
   /* Video projects use GSVideo, see VIDEO.md for more info. */
