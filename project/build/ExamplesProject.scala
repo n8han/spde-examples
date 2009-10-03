0bc150a80b3bd370b03cd0b1b190b687ba841a0d
Nathan Hamblen <nathan@technically.us>
2009-10-02 20:10:40 -0400

    use new default projects, fixes

--- project/build/ExamplesProject.scala
+++ project/build/ExamplesProject.scala
@@ -3,142 +3,15 @@ import spde._
 
 class ExamplesProject(info: ProjectInfo) extends ParentProject(info)
 {
-  lazy val explode = project("Explode", "Explode", new SpdeProject(_) with AppletProject)
-  lazy val flocking = project("Flocking", "Flocking", new SpdeProject(_) with AppletProject)
-  lazy val fold = project("Fold", "Fold", new SpdeProject(_) with AppletProject)
-  lazy val list = project("List", "List", new SpdeProject(_) with AppletProject)
-  lazy val gasket = project("Sierpinski", "Sierpinski_Gasket", new SpdeProject(_) with AppletProject)
-  lazy val lsystems = project("L-Systems", "L_Systems", new SpdeProject(_) with AppletProject)
-  lazy val matrix = project("Matrix", "Matrix", new SpdeOpenGLProject(_) with AppletProject)
+  lazy val explode = project("Explode", "Explode", new DefaultSpdeProject(_))
+  lazy val flocking = project("Flocking", "Flocking", new DefaultSpdeProject(_))
+  lazy val fold = project("Fold", "Fold", new DefaultSpdeProject(_))
+  lazy val list = project("List", "List", new DefaultSpdeProject(_))
+  lazy val gasket = project("Sierpinski", "Sierpinski_Gasket", new DefaultSpdeProject(_))
+  lazy val lsystems = project("L-Systems", "L_Systems", new DefaultSpdeProject(_))
+  lazy val matrix = project("Matrix", "Matrix", new DefaultOpenGLProject(_))
 
   /* Video projects use GSVideo, see VIDEO.md for more info. */
   lazy val loop = project("Loop", "Loop", new SampleVideoProject(_))
   lazy val scratchP = project("Scratch", "Scratch", new SampleVideoProject(_))
-}
-
-trait AppletProject extends SpdeProject with archetect.TemplateTasks
-{
-  // this project's Proguard invocation tasks are derived from sbt.LoaderProject
-  import java.io.File
-  val appletOutput = outputPath / "applet"
-  val proguardConfigurationPath: Path = outputPath / "proguard.pro"
-  def outputExt(ext: String) = name + "-applet-" + version + "." + ext
-
-  lazy val outputJar = appletOutput / outputExt("jar")
-  lazy val outputJp = outputPath / outputExt("jp")
-  lazy val outputJpgz = appletOutput / outputExt("jpgz")
-  lazy val outputHtml = appletOutput / (name + ".html")
-  
-  def rootProjectDirectory = rootProject.info.projectPath
-
-  /****** Dependencies  *******/
-  val defaultConfig = config("default")
-  val toolsConfig = config("tools")
-  val proguardJar = "net.sf.proguard" % "proguard" % "4.3" % "tools->default"
-  
-  /******** Proguard *******/
-  lazy val proguard = proguardTask dependsOn(`package`, glob, writeProguardConfiguration)
-  lazy val writeProguardConfiguration = writeProguardConfigurationTask dependsOn `package`
-  lazy val pack = packTask dependsOn(proguard)
-  lazy val writeHtml = writeHtmlTask dependsOn(glob)
-  lazy val applet = appletTask dependsOn (pack, writeHtml)
-  
-  private def proguardTask = fileTask(outputJar from sourceGlob)
-    {
-      FileUtilities.clean(outputJar :: Nil, log)
-      val proguardClasspath = managedClasspath(toolsConfig)
-      val proguardClasspathString = Path.makeString(proguardClasspath.get)
-      val configFile = proguardConfigurationPath.asFile.getAbsolutePath
-      val exitValue = Process("java", List("-Xmx128M", "-cp", proguardClasspathString, "proguard.ProGuard", "@" + configFile)) ! log
-      if(exitValue == 0) None else Some("Proguard failed with nonzero exit code (" + exitValue + ")")
-    }
-  private def renderInfo = {
-    // tasks that call here should depend on glob
-    val sz = """\s*size\s*\(\s*(\d+)\s*,\s*(\d+),?\s*(\S*)\s*\)\s*""".r
-    val sz_line = io.Source.fromFile(sourceGlob.asFile).getLines.find(None != sz.findFirstIn(_))
-    val rendererMap = Map (
-      "P3D" -> "processing.core.PGraphics3D",
-      "JAVA2D" -> "processing.core.PGraphicsJava2D",
-      "OPENGL"-> "processing.opengl.PGraphicsOpenGL",
-      "PDF"-> "processing.pdf.PGraphicsPDF",
-      "DXF"-> "processing.dxf.RawDXF"
-    )
-    val univRenderers = "processing.core.PGraphicsJava2D" :: Nil
-    
-    sz_line match {
-      case Some(sz(width, height, "")) => (width, height, univRenderers)
-      case Some(sz(width, height, r)) => (width, height, rendererMap(r) :: univRenderers)
-      case Some(sz(width, height)) => (width, height, univRenderers)
-      case _ => (100, 100, univRenderers)
-    }
-  }
-  private def writeProguardConfigurationTask = 
-    fileTask(proguardConfigurationPath from sourceGlob)
-    {
-      // the template for the proguard configuration file
-      val outTemplate = """
-        |-dontobfuscate
-        |-dontnote
-        |-dontwarn
-        |-libraryjars %s
-        |%s
-        |-outjars %s
-        |-ignorewarnings
-        |-keep class com.sun.opengl.impl.** { *** *(***); }
-        |-keep class %s
-        |-keep class MyRunner { *** main(...); }
-        |-keep class processing.core.PApplet { *** main(...); }
-        |-keep class spde.core.SApplet { *** scripty(...); }
-        |-keepclasseswithmembers class * { public void dispose(); }
-        |-keep public class org.apache.commons.logging.impl.LogFactoryImpl
-        |-keep public class org.apache.commons.logging.impl.Jdk14Logger { *** <init>(...); }
-        |-keep public class net.databinder.dispatch.Http* { scala.Function1 ok(); }
-        |"""
-      
-      val defaultJar = (outputPath / defaultJarName).asFile.getAbsolutePath
-      log.debug("proguard configuration using main jar " + defaultJar)
-      val externalDependencies = (mainCompileConditional.analysis.allExternals).map(_.getAbsoluteFile).filter(_.getName.endsWith(".jar"))
-      log.debug("proguard configuration external dependencies: \n\t" + externalDependencies.mkString("\n\t"))
-      // partition jars from the external jar dependencies of this project by whether they are located in the project directory
-      // if they are, they are specified with -injars, otherwise they are specified with -libraryjars
-      val (externalJars, libraryJars) = externalDependencies.toList.partition(jar => Path.relativize(rootProjectDirectory, jar).isDefined)
-      log.debug("proguard configuration library jars locations: " + libraryJars.mkString(", "))
-      // exclude properties files and manifests from scala-library jar
-      val inJars = (defaultJar :: externalJars.map( _ + "(!META-INF/**,!*.txt)")).map("-injars " + _).mkString("\n")
-      
-      val (width, height, renderers) = renderInfo
-      
-      val proguardConfiguration =
-        outTemplate.stripMargin.format(libraryJars.mkString(File.pathSeparator),
-          inJars, outputJar.absolutePath, name) + renderers.map { renderer =>
-            "-keep class %s { *** <init>(...); }\n" format renderer
-          }.mkString
-      log.debug("Proguard configuration written to " + proguardConfigurationPath)
-      FileUtilities.write(proguardConfigurationPath.asFile, proguardConfiguration, log)
-    }
-    def packTask = fileTask(outputJpgz from outputJar) {
-      Pack.pack(outputJar, outputJp, log) orElse
-        FileUtilities.gzip(outputJp, outputJpgz, log)
-    }
-    def templateMappings = renderInfo match {
-      case (width, height, _)  => Map(
-        "width" -> width, 
-        "height" -> height,
-        "sketch" -> name,
-        "archive" -> outputJar.asFile.getName
-      )
-    }
-    def writeHtmlTask = templateTask(AppletTemplate.resource, outputHtml)
-    
-    def appletTask = task {
-      try {
-        val dsk = Class.forName("java.awt.Desktop")
-        dsk.getMethod("browse", classOf[java.net.URI]).invoke(
-          dsk.getMethod("getDesktop").invoke(null), outputHtml.asFile.toURI
-        )
-        None
-      } catch {
-        case _ => Some("Unable to open browser. Java 5 VM?")
-      }
-    }
 }
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,5 +2,4 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
 	val spde_sbt = "net.databinder.spde" % "spde-sbt-plugin" % "0.1.4-SNAPSHOT"
-	val archetect = "net.databinder" % "archetect-plugin" % "0.1.2-SNAPSHOT"
 }
