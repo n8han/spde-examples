eab92bbec48929e89165c2d7afa00d6e3330bc13
Nathan Hamblen <nathan@technically.us>
2009-11-22 23:24:10 -0500

    dispatch version, 2.8 compatibility

--- project/build/ExamplesProject.scala
+++ project/build/ExamplesProject.scala
@@ -17,7 +17,7 @@ class ExamplesProject(info: ProjectInfo) extends ParentProject(info)
 
 
   lazy val trending = project("Trending", "Trending", new DefaultSpdeProject(_) {
-    val dispatch = "net.databinder" %% "dispatch-lift-json" % "0.6.0"
+    val dispatch = "net.databinder" %% "dispatch-lift-json" % "0.6.2"
   })
   lazy val straight_scala = project("Straight_Scala", "Straight_Scala", new DefaultSpdeProject(_))
   lazy val straight_java = project("Straight_Java", "Straight_Java", new DefaultSpdeProject(_))
