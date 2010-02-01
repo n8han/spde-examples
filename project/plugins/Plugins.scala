6612c1430782891de0b5e23c1b72624b001500a0
Nathan Hamblen <nathan@technically.us>
2010-01-31 20:42:14 -0500

    version updates

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,5 +2,5 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val databinder_repo = "Databinder Repository" at "http://databinder.net/repo"
-	val spde_sbt = "us.technically.spde" % "spde-sbt-plugin" % "0.2.2"
+	val spde_sbt = "us.technically.spde" % "spde-sbt-plugin" % "0.2.3"
 }
