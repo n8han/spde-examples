cee7bbda31368ec3acc6632c15e295e819dc09fc
Nathan Hamblen <nathan@technically.us>
2010-08-22 11:50:28 -0400

    plugin versions

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,10 +2,11 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val databinder_repo = "Databinder Repository" at "http://databinder.net/repo"
-	val spde_sbt = "us.technically.spde" % "spde-sbt-plugin" % "0.4.0"
-	
-  val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
-	val posterous = "net.databinder" % "posterous-sbt" % "0.1.3"
+  val spde_sbt = "us.technically.spde" % "spde-sbt-plugin" % "0.4.1"
 
-  val android = "org.scala-tools.sbt" % "android-plugin" % "0.4.1"
+  val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
+  val snuggletex_repo = "snuggletex_repo" at "http://www2.ph.ed.ac.uk/maven2"
+  val posterous = "net.databinder" % "posterous-sbt" % "0.1.6"
+
+  val android = "org.scala-tools.sbt" % "sbt-android-plugin" % "0.5.0"
 }
