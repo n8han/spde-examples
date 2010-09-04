fd3d27a647771afcef4a7e2182e21a8265d53d8c
philogb <philogb@gmail.com>
2010-09-04 23:00:04 +0800

    Add FractalParticles example to the ExamplesProject build

--- project/build/ExamplesProject.scala
+++ project/build/ExamplesProject.scala
@@ -18,7 +18,8 @@ class ExamplesProject(info: ProjectInfo) extends ParentProject(info) with poster
   lazy val ti_81 = project("TI-81", "TI-81", new DefaultSpdeProject(_))
   lazy val geometry = project("Geometry", "Geometry", new DefaultOpenGLProject(_))
   lazy val esfera = project("Esfera", "Esfera", new DefaultOpenGLProject(_))
-
+  lazy val fractalParticles = project("FractalParticles", "FractalParticles", new DefaultOpenGLProject(_))
+  
   /* Video projects use GSVideo, see VIDEO.md for more info. */
   lazy val loop = project("Loop", "Loop", new SampleVideoProject(_))
   lazy val scratchP = project("Scratch", "Scratch", new SampleVideoProject(_))
